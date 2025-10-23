385e6c2d9e599ea0f8f1d763065feaf7ce29ba12
riteshvinayak9 <riteshvinayak9@gmail.com>
2025-10-23 00:12:57 -0700

    Updated the UI of Login Page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
         {
           state === 'Sign Up' && (
             <div className='w-full'>
-              <p>Full Name</p>
+              <p>Full_Name</p>
               <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(e) => setName(e.target.value)} value={name} required />
             </div>
           )
